bfafec885dc8d171ccc9a7a7ce5d3f48da4240fd
eley <laombac14@gmail.com>
2025-10-27 13:15:31 +0800

    new envs

---
+++ frontend/app/component/header/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Page() {
+    return (
+        <header className="w-full justify-center align-center flex flex-col items-center gap-[5vw] mb-[5vw]">
+            <h1 className="font-bold text-[2vw]">Welcome to Track Assessment</h1>
+        </header>
+    );
+}
