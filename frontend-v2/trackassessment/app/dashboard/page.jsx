7dc23a788e072189731167e41240b4ede3a5e292
eley <laombac14@gmail.com>
2026-01-12 23:20:50 +0800

    fastapi files

--- frontend-v2/trackassessment/app/dashboard/page.jsx
+++ frontend-v2/trackassessment/app/dashboard/page.jsx
@@ -8,9 +8,9 @@ export default async function Dashboard(){
     const {data} = await supabase.auth.getUser();
 
     return(
-        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-            <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-                <h1 className="text-5xl">Welcome to your Dashboard, {data.user.email}</h1>
+        <div className="flex bg-transparent">
+            <main className="flex justify-center">
+                <h1 className="text-stone-700/70 text-2xl">Dashboard</h1>
             </main>
         </div>
     )
